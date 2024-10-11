88d5a21dde089f4d8a4ae2b29243888ceef365fc
Carlos César de O. Fonseca <73307575+CarCesar@users.noreply.github.com>
2024-10-11 13:41:55 -0300

    initial Chapter4-Ex-1.lean

--- Fad/Chapter4-Ex-1.lean
+++ Fad/Chapter4-Ex-1.lean
@@ -1,30 +1,7 @@
 import Fad.Chapter4
-import Fad.Chapter3
 
 namespace Chapter4
 
 /- complete here -/
-def partition3 (y : Nat) (xs : List Nat) : (List Nat × List Nat × List Nat) :=
-  xs.foldr (λ x (us_vs_ws : List Nat × List Nat × List Nat) =>
-    let (us, vs, ws) := us_vs_ws
-    if x < y then (x :: us, vs, ws)
-    else if x = y then (us, x :: vs, ws)
-    else (us, vs, x :: ws))
-  ([], [], [])
-
-#eval partition3 3 [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
-
-/-
-def partition3_geral {α : Type} (y : α) (xs : List α) : (List α × List α × List α) :=
-  xs.foldr (λ x (us_vs_ws : List α × List α × List α) =>
-    let (us, vs, ws) := us_vs_ws
-    if x < y then (x :: us, vs, ws)
-    else if x = y then (us, x :: vs, ws)
-    else (us, vs, x :: ws))
-  ([], [], [])
-
-Quando utilizo o geral, não estou conseguindo utilizar "<" ou "="
--/
-
 
 end Chapter4
