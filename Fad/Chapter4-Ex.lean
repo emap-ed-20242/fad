52856ec7cd2439ce594a15d3706af00c685f0d74
Alexandre Rademaker <arademaker@gmail.com>
2024-10-20 19:43:18 -0300

    correção exercícios

--- Fad/Chapter4-Ex.lean
+++ Fad/Chapter4-Ex.lean
@@ -13,6 +13,56 @@ But for t = 1024 and f x = x^2 below f x = t and f (x + 1) > t
 #eval (fun x => dbg_trace "fun {x}"; x * x) 33
 
 
+/- 4.6 -/
+
+#eval D2.search₁ (λ (x, y) => x ^ 3 + y ^ 3) 1729
+
+
+/- 4.7 -/
+
+def Tree1.Tree.flatcat : (t : Tree1.Tree a) → (xs: List a) → List a
+| null, xs => xs
+| (node l x r), xs => l.flatcat (x :: r.flatcat xs)
+
+def Tree1.Tree.flatten₁ (t : Tree1.Tree a) : List a :=
+ t.flatcat []
+
+#eval Tree1.mkTree [1,2,3,5] |>.flatten
+#eval Tree1.mkTree [1,2,3,5] |>.flatten₁
+
+example (t: Tree1.Tree a) :
+  t.flatten = t.flatten₁ := by
+  induction t with
+  | null => exact rfl
+  | node l x r ihl ihr =>
+    simp [Tree1.Tree.flatten₁]
+    simp [Tree1.Tree.flatten]
+    simp [Tree1.Tree.flatcat]
+    simp [ihl, ihr]
+    simp [Tree1.Tree.flatten₁]
+    sorry
+
+
+/- 4.8
+  obs: pode ser necessario mathlib? -/
+
+open Chapter4.Tree1.Tree in
+
+example {α : Type} (t : Chapter4.Tree1.Tree α) :
+  t.height ≤ t.size ∧ t.size < 2 ^ t.height := by
+ apply And.intro
+ {
+ induction t with
+ | null =>
+   simp [height, size]
+ | node t₁ x t₂ ihl ihr =>
+   dsimp [height, size]
+   sorry
+ }
+ {
+  sorry
+ }
+
 /- 4.17 -/
 
 section
