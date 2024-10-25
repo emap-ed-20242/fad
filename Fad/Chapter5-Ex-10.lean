4663407634f21c0874dc65302b7cf9c226456967
Wellington36 <wellington.71319@gmail.com>
2024-10-25 11:35:08 -0300

    Finish exercice 5.10

--- Fad/Chapter5-Ex-10.lean
+++ Fad/Chapter5-Ex-10.lean
@@ -2,31 +2,27 @@ import Fad.Chapter5
 
 namespace Chapter5
 
-/- 5.10 : see book -/
--- Expression 1: flip (foldl (λ f x, (x ∘ f)) id) []
--- Define the reverse function using foldl and flip (foldl (flip (:)) [])
-def reverse_via_foldl {α : Type} : list α → list α
-| [] := []
-| xs := foldl (flip (::)) [] xs
+/- 5.10 -
+Primeiro analizando as expressoes em lean
+-/
+def expression₁ {α : Type} (xs : List α) : List α :=
+  flip (List.foldl (λ f x => (λ acc => x :: (f acc))) id) [] xs
 
--- Define the reverse function using foldr (this works too, but foldl is preferred for reverse)
-def reverse_via_foldr {α : Type} : list α → list α
-| [] := []
-| xs := foldr (λ x acc, acc ++ [x]) [] xs
+def expression₂ {α : Type} (xs : List α) : List α :=
+  flip (List.foldr (λ x f => (λ acc => (f acc) ++ [x])) id) [] xs
 
--- Example to check that both definitions are correct and equivalent to list.reverse
-example (xs : list ℕ) : reverse_via_foldl xs = xs.reverse :=
-begin
-  rw reverse_via_foldl,
-  refl, -- reverse_via_foldl is equivalent to reverse
-end
+#eval expression₁ [1, 2, 3, 4]
 
-example (xs : list ℕ) : reverse_via_foldr xs = xs.reverse :=
-begin
-  rw reverse_via_foldr,
-  refl, -- reverse_via_foldr is equivalent to reverse
-end
+#eval expression₂ [1, 2, 3, 4]
 
+/-
+Temos que ambas as expressoes representam o reverse, uma outra forma de
+implementar o reverse é
+-/
 
+def reverse {α : Type} : List α → List α :=
+  List.foldl (λ acc x => x :: acc) []
+
+#eval reverse [1, 2, 3, 4]
 
 end Chapter5
