6807af45eb764c9d8890a414bfcdf70d5e7ad43f
nmspaniol <nicolasmspaniol@gmail.com>
2024-11-01 12:40:34 -0300

    Terminação provada

--- Fad/Chapter5-Ex17.lean
+++ Fad/Chapter5-Ex17.lean
@@ -26,24 +26,23 @@ instance [ToString a] : Repr (Tree a) where
 --------------------------------------------------------------
 def mkPair : Nat → (List a) → (Tree a × List a)
   | _, [] => (Tree.null, [])
-  | 0, xs => (Tree.null, xs)
   | n, x :: xs =>
-    let m := (n - 1) / 2
-    let y := mkPair m xs
-    have h1 : sizeOf (mkPair m xs).2 ≤ sizeOf xs := by sorry      
-    let z := mkPair (n - 1 - m) y.snd
-    (Tree.node x y.fst z.fst, z.snd)
+    if h : n = 0 then (Tree.null, x :: xs) else
+      let m := (n - 1) / 2
+      have : m < n := by
+        rw [show ¬n = 0 ↔ n > 0 by exact Nat.ne_zero_iff_zero_lt] at h
+        rw [Nat.div_lt_iff_lt_mul Nat.zero_lt_two]
 
-  termination_by n as => as
-  decreasing_by
-    rw [List.cons.sizeOf_spec, Nat.add_comm]
-    simp [Nat.lt_add_one_iff]
-    simp
-    have h2 : sizeOf y.snd < sizeOf (x :: xs) := by
-      simp
-      rw [Nat.add_comm]
-      exact Nat.lt_add_one_of_le h1
-    exact h2
+        calc
+          n - 1 < n := Nat.sub_one_lt_of_lt h
+          _ < n + n := Nat.lt_add_of_pos_right h
+          _ = n * 2 := Eq.symm (Nat.mul_two n)
+        
+      let y := mkPair m xs
+      let z := mkPair (n - 1 - m) y.snd
+      have : (n - 1 - m) < n := by exact Nat.sub_one_sub_lt_of_lt this
+      (Tree.node x y.fst z.fst, z.snd)
+      termination_by n _ => n
 
 def mkTree (xs : List a) : Tree a := (mkPair xs.length xs).1
 --------------------------------------------------------------
