e9714c4d8bbab574bf164085ab962c622f170f2a
Alexandre Rademaker <arademaker@gmail.com>
2024-11-08 16:28:29 -0300

    códigos AoC

--- AoC/AoC_2018/day1.lean
+++ AoC/AoC_2018/day1.lean
@@ -3,47 +3,36 @@
 import Std.Data.HashSet
 
 namespace AoC2018D1
+
 -- PART 1:
 
-def input_day1 : String := include_str "../../data/AoC2018_day1.txt"
-
-def remove_line_feed (s : String) : String :=
-  s.replace "\x0d" ""
-
-def split_by_new_line (s : String) : List String :=
-  s.split (· == '\n')
+def content : String := include_str "../../data/AoC2018_day1.txt"
 
 def parse_ints (lst : List String) : List Int :=
   lst.filterMap (fun s =>
-    if s.startsWith "+" then
-      s.drop 1 |>.toInt?
-    else
-      s.toInt?)
+    (if s.startsWith "+" then s.drop 1 else s).toInt?)
 
-def input_task := parse_ints <| split_by_new_line <| remove_line_feed <| input_day1
+def input : List Int := parse_ints <| content.splitOn "\n"
 
-#eval input_task
-
-def sum_of_ints (lst : List Int) : Int :=
-  lst.foldl (fun acc x => acc + x) 0
-
-#eval sum_of_ints <| input_task -- 497
+#eval input.foldl (fun acc x => acc + x) 0
 
 -- PART 2:
 
--- depending on the input, the function may not terminate, hence why we can't prove termination
-partial def find_first_duplicate_cumulative_sum (lst : List Int) : Int :=
-  let rec aux (seen : Std.HashSet Int) (current_sum : Int) (remaining : List Int) (original : List Int) : Int :=
-    match remaining with
-    | [] => aux seen current_sum original original
-    | h :: t =>
-      let new_sum := current_sum + h
+-- depending on the input, the function may not terminate, hence why
+-- we cant prove termination
+
+partial def first_duplicate_freq (lst : List Int) : Int :=
+  let rec aux (seen : Std.HashSet Int) (acc : Int) (rem : List Int) : Int :=
+    match rem with
+    | [] => aux seen acc lst
+    | h :: ts =>
+      let new_sum := acc + h
       if seen.contains new_sum then
         new_sum
       else
-        aux (seen.insert new_sum) new_sum t original
-  aux (Std.HashSet.empty.insert 0) 0 lst lst
+        aux (seen.insert new_sum) new_sum ts
+  aux (Std.HashSet.empty.insert 0) 0 lst
 
-#eval find_first_duplicate_cumulative_sum <| input_task -- 558
+#eval first_duplicate_freq <| input
 
 end AoC2018D1
