6940c8d6c50621aa20ca2645f7b1cdc9ba039e0d
patrick20022 <patrick.saul00@gmail.com>
2024-11-13 14:29:42 -0300

    Melhorando o código

--- AoC/AoC_2017/day1.lean
+++ AoC/AoC_2017/day1.lean
@@ -2,41 +2,41 @@
 
 namespace AoC2017D1
 
-def input_day1 : String := include_str "../../data/AoC2017_day1.txt"
+def inp : String := include_str "../../data/AoC2017_day1.txt"
 
-def remove_line_feed (s : String) : String :=
+def rmLf (s : String) : String :=
   s.replace "\x0d" ""
 
-def input_task := remove_line_feed input_day1
+def inpStr := rmLf inp
 
 -- Parte 1
-def ChToDig (c : Char) : Nat :=
+def chToDig (c : Char) : Nat :=
   if '0' ≤ c ∧ c ≤ '9' then c.val.toNat - '0'.val.toNat else 0
 
-def capSol (inp : String) : Nat :=
-  let ch := inp.toList
+def solve1 (str : String) : Nat :=
+  let ch := str.toList
   let len := ch.length
-  let res := ch.enum.foldl (fun acc (i, curr) =>
+  let res := ch.enum.foldl (fun acc (i, cur) =>
     let nxt := ch[(i + 1) % len]!
-    let crrDig := ChToDig curr
-    let nxtDig := ChToDig nxt
-    if crrDig == nxtDig then acc + crrDig else acc
+    let curDig := chToDig cur
+    let nxtDig := chToDig nxt
+    if curDig == nxtDig then acc + curDig else acc
   ) 0
   res
 
-#eval capSol input_task
+#eval solve1 inpStr
 
 -- Parte 2
-def capSol2 (inp : String) : Nat :=
-  let ch := inp.toList
+def solve2 (str : String) : Nat :=
+  let ch := str.toList
   let len := ch.length
-  let stp := len / 2
-  let res := ch.enum.foldl (fun acc (i, crr) =>
-    let nxt := ch[(i + stp) % len]!
-    let crrDig := ChToDig crr
-    let nxtDig := ChToDig nxt
-    if crrDig == nxtDig then acc + crrDig else acc
+  let step := len / 2
+  let res := ch.enum.foldl (fun acc (i, cur) =>
+    let nxt := ch[(i + step) % len]!
+    let curDig := chToDig cur
+    let nxtDig := chToDig nxt
+    if curDig == nxtDig then acc + curDig else acc
   ) 0
   res
 
-#eval capSol2 input_task
+#eval solve2 inpStr
