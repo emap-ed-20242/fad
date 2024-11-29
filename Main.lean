5875d2f1f3854d9a1c35393ba767fd373b412fd5
Alexandre Rademaker <arademaker@gmail.com>
2024-11-29 00:35:34 -0300

    small change in the Main for debug

--- Main.lean
+++ Main.lean
@@ -13,5 +13,5 @@ def main : IO Unit :=
   let c₁ := solve applyAction₁ initGrid₁ c
   let c₂ := solve applyAction₂ initGrid₂ c
   -- IO.println s!"c₁: {count (λ c => cond c 1 0) c₁} c₂: {count id c₂}"
-  -- IO.FS.writeFile (System.FilePath.mk "day6-1.pgm") (grid2pgm c₁ (λ c => cond c 1 0))
-  IO.FS.writeFile (System.FilePath.mk "day6-2.pgm") (grid2pgm c₂ id)
+  IO.FS.writeFile (System.FilePath.mk "day6-1.pgm") (grid2pgm c₁ (λ c => cond c 0 1))
+  -- IO.FS.writeFile (System.FilePath.mk "day6-2.pgm") (grid2pgm c₂ id)
