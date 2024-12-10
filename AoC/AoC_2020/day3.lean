10867d4061fa7147a86c3fb40fdda04ab1ce9e3d
Alexandre Rademaker <arademaker@gmail.com>
2024-12-10 14:05:31 -0300

    review AoC 2020 day 3

--- AoC/AoC_2020/day3.lean
+++ AoC/AoC_2020/day3.lean
@@ -1,54 +1,39 @@
-/-!
-# Problem: https://adventofcode.com/2020/day/3
--/
+
+-- Problem: https://adventofcode.com/2020/day/3
 
 namespace AoC2020D3
 
-def raw_data : String := include_str "../../data/AoC2020_day3.txt"
+def content : String := include_str "../../data/AoC2020_day3.txt"
 
-def process_data (s : String) : List (String) :=
-  s.replace "\x0d\n" "$" |>.replace "\n" "$" |>.splitOn "$"
+def input : List (String) :=
+  content.splitOn "\n" |>.map String.trim
 
-def grid : List String := process_data raw_data
+-- Part 1
 
-#eval raw_data
-#eval grid
-
--------------- Part one --------------
-
-def solve₁! (ls : List String) : Nat :=
-  dfs! ls 0
-  where dfs! (ls : List String) (y : Nat) : Nat :=
+def dfs (ls : List String) (dx dy : Nat) (x : Nat) : Nat :=
     match ls with
     | [] => 0
-    | l :: ls' =>
-      let c := l.get! ⟨y⟩
-      let y' := (y + 3) % l.length
-      (if c = '#' then 1 else 0) + dfs! ls' y'
+    | l::ls' =>
+      let x' := (x + dx) % l.length
+      let rs := ls'.drop (dy - 1)
+      cond (l.get ⟨x⟩ = '#') 1 0 + dfs rs dx dy x'
+termination_by (ls.length, x)
 
-#eval solve₁! grid -- 228 solution part 1
+def solve₁ (ls : List String) : Nat :=
+  dfs ls 3 1 0
 
--------------- Part two --------------
+#eval solve₁ input
 
-def solve₂! (ls : List String) (dx dy : Nat): Nat :=
-  dfs! ls 0 0
-  where dfs! (ls : List String) (aux y : Nat) : Nat :=
-    match ls with
-    | [] => 0
-    | l :: ls' =>
-      match aux with
-      | 0 =>
-        let c := l.get! ⟨y⟩
-        let y' := (y + dy) % l.length
-        (if c = '#' then 1 else 0) + dfs! ls' dx y'
-      | aux'+1 => dfs! ls' aux' y
+-- Part 2
 
-#eval solve₂! grid (1-1) 1 -- 84
-#eval solve₂! grid (1-1) 3 -- 228 (equivalent to solve₁!)
-#eval solve₂! grid (1-1) 5 -- 89
-#eval solve₂! grid (1-1) 7 -- 100
-#eval solve₂! grid (2-1) 1 -- 40
+def solve₂ (ls : List String) (dx dy : Nat) : Nat :=
+  dfs ls dx dy 0
 
-#eval 84 * 228 * 89 * 100 * 40 -- 6818112000 solution part 2
+#eval [(1,1),(3,1),(5,1),(7,1),(1,2)].map
+  (λ ⟨dx,dy⟩ => solve₂ input dx dy)
+
+#eval [(1,1),(3,1),(5,1),(7,1),(1,2)].foldl
+  (λ acc ⟨dx,dy⟩ => acc * solve₂ input dx dy) 1
+
 
 end AoC2020D3
