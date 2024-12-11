027dd251b15e99927c02150bd42eadb6c727294b
Alexandre Rademaker <arademaker@gmail.com>
2024-12-11 12:51:25 -0300

    review 2021 days 1 e 2

--- AoC/AoC_2021/day1.lean
+++ AoC/AoC_2021/day1.lean
@@ -1,75 +1,59 @@
+
+/- # Problema https://adventofcode.com/2021/day/1 -/
+
 namespace AoC2021D1
 
-
--- Data process --
 def content : String := include_str "../../data/AoC2021_day1.txt"
 
-def process_data (s : String) : List Nat :=
-  s.replace "\x0d\n" " " |>.replace "\n" " "
-  |>.splitOn " " |>.map String.toNat!
-
-def convert_to_nat_list : List Nat := process_data content
-
-#eval content
-#eval convert_to_nat_list
+def input : List Nat :=
+  content.splitOn "\n" |>.filter (· ≠ "") |>.map (·.trim.toNat!)
 
 
--- Part 1 --
+/- # Part 1
 
-/-!
 Analisar uma sequência de medições de profundidade do mar.
 
-- O input é uma lista de números inteiros, representando as profundidades do mar, coletados em diferentes momentos.
+- O input é uma lista de números inteiros, representando as
+  profundidades do mar, coletados em diferentes momentos.
 
-Objetivo:
-Contar o número de vezes que uma medição de profundidade é maior que a medição anterior.
+Objetivo: Contar o número de vezes que uma medição de profundidade é
+maior que a medição anterior.
+
 -/
-
 
 def countIncreases : List Nat → Nat
-| [] => 0
+| []  => 0
 | [_] => 0
 | (x :: y :: xs) => (if y > x then 1 else 0) + countIncreases (y :: xs)
-/-!
-Da para melhorar e implementar a função de alta ordem foldr ou flodl com a lógica contrária de decaimento.
-Mas por hora ta bom o suficiente :)
--/
-#eval countIncreases convert_to_nat_list
+
+#eval countIncreases input
 
 
--- Part 2 --
+/- # Part 2
 
-/-!
-Análise de somas de janelas deslizantes de três medições consecutivas de profundidade do mar.
+Análise de somas de janelas deslizantes de três medições consecutivas
+de profundidade do mar.
 
-- Em vez de comparar as medições individuais, agora deve-se considerar janelas de três medições consecutivas.
+- Em vez de comparar as medições individuais, agora deve-se considerar
+  janelas de três medições consecutivas.
+
 - Para cada janela, a soma das três medições é calculada.
 
-Objetivo:
-Contar o número de vezes que a soma de uma janela de três medições consecutivas é maior que a soma da janela anterior.
+Objetivo: Contar o número de vezes que a soma de uma janela de três
+medições consecutivas é maior que a soma da janela anterior.
+
 -/
 
+def sublists (n: Nat) : List α → List (List α) → List (List α)
+ | []     , acc => acc.reverse
+ | x :: xs, acc =>
+   let slice := List.take n (x :: xs)
+   if slice.length < 3 then
+     acc.reverse
+   else
+     sublists n xs (slice :: acc)
 
-def sum : List Nat -> Nat
-| [] => 0
-| (x :: xs) => x + sum xs
+#eval sublists 3 input []
+#eval countIncreases $ sublists 3 input [] |>.map List.sum
 
-def movingWindow (n:Nat) (acc:List Nat) : List Nat -> List Nat
-| [] => []
-| (x :: xs) => (sum (List.take n (x :: acc))) :: movingWindow n (x :: acc) xs
-/-!
-Vou criando uma lista com a soma das sublistas de tamanho n (3).
-Para isso, crio as janelas deslizantes de 3 elementos consecutivos, somo os valores de cada janela e comparo as somas de janelas consecutivas.
-
-- Funciona para qualquer tamanho de janela que queiramos. Inclusive com janela = 1 resolvendo a anterior também.
--/
-
-def countMovingIncreases3 ( xs: List Nat) : Nat := countIncreases (movingWindow 3 (List.take 2 xs) (List.drop 2 xs))
-/-!
-- List.take: n xs retorna os primeiros n elementos da lista xs.
--/
-#eval countMovingIncreases3 convert_to_nat_list
-
-
-def countMovingIncreases1 ( xs: List Nat) : Nat := countIncreases (movingWindow 1 (List.reverse $ List.take 2 xs) (List.drop 2 xs)) + 1
-#eval countMovingIncreases1 convert_to_nat_list
+end AoC2021D1
--- AoC/AoC_2021/day2.lean
+++ AoC/AoC_2021/day2.lean
@@ -1,23 +1,49 @@
+import Std.Internal.Parsec
+
+/- # Problema https://adventofcode.com/2021/day/2 -/
 
 namespace AoC2021D2
 
 def content : String := include_str "../../data/AoC2021_day2.txt"
 
+structure Command where
+  direction : String
+  unit : Nat
+deriving Repr, Inhabited
 
-def process_data (s : String) : List Int :=
-  s.replace "\x0d\n" " " |>.replace "\n" " "
-  |>.splitOn " " |>.map String.toInt!
+def parseCommand (s : String) : Command :=
+  match s.trim.splitOn " " with
+  | [d, u] =>
+    { direction := d, unit := u.toNat! }
+  | _ =>
+    { direction := "", unit := 0 }
 
-#eval content
+def input : List Command :=
+  content.splitOn "\n" |>.filter (· ≠ "") |>.map parseCommand
 
-inductive Command
-| Forward
-| Down
-| Up
+-- Part 1
+
+def execute₁ (pos : Nat × Nat) : List Command → Nat × Nat
+ | [] => pos
+ | c :: xs =>
+    match c with
+    | ⟨"forward", n⟩ => execute₁ (pos.1 + n, pos.2) xs
+    | ⟨"down", n⟩    => execute₁ (pos.1, pos.2 + n) xs
+    | ⟨"up", n⟩      => execute₁ (pos.1, pos.2 - n) xs
+    | ⟨_,_⟩          => execute₁ pos xs
+
+#eval (λ a => a.1 * a.2) $ execute₁ (0,0) input
 
 
-def parseCmd : String → Command
-| "forward" => Command.Forward
-| "down" => Command.Down
-| "up" => Command.Up
-| _ => Command.Forward
+-- Part 2
+
+def execute₂ (pos : Nat × Nat) (aim : Nat) : List Command → Nat × Nat
+ | [] => pos
+ | c :: xs =>
+    match c with
+    | ⟨"forward", n⟩ => execute₂ (pos.1 + n, pos.2 + aim * n) aim xs
+    | ⟨"down", n⟩    => execute₂ pos (aim + n) xs
+    | ⟨"up", n⟩      => execute₂ pos (aim - n) xs
+    | ⟨_,_⟩          => execute₂ pos aim xs
+
+#eval (λ a => a.1 * a.2) $ execute₂ (0,0) 0 input
